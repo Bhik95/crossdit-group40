00e57be0ef8c220faa42c3caa8eeae77e4341588
Francesco Frassineti <frassybhik@gmail.com>
2020-10-29 00:47:58 +0100

    Express tutorial setup

--- crossdit-group40-app/src/App.js
+++ crossdit-group40-app/src/App.js
@@ -1,46 +1,66 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Todos from './components/Todos'
+import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
+class App extends React.Component {
+  state = {
+    todos: []
   }
 
   componentDidMount(){
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({todos: res.data}))
   }
 
-  componentWillUnmount(){
-    clearInterval(this.timerID);
+  // Toggle completion
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })})
+  }
+
+  delTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]}))
+    
+  }
+
+  addTodo = (title) => {
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title,
+      completed: false
+    })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data]}))
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header/>
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+              </React.Fragment>
+            )}/>
+            <Route path="/about" component={About}/>
+          </div>
+        </div>
+      </Router>
+      
     );
   }
-
-  tick(){
-    this.setState({
-      date: new Date()
-    });
-  }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-      <Clock></Clock>
-    </div>
-  );
 }
 
 export default App;
